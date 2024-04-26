f1a8f73f27fc5fcedced50608279d1315051185f
Kristin Huot <kristin.huot@gmail.com>
2024-04-26 16:42:24 -0500

    added photos

--- database.sql
+++ database.sql
@@ -9,5 +9,10 @@ CREATE TABLE "gallery" (
 INSERT INTO "gallery" 
 ("url", "title", "description")
 VALUES
-('images/goat_small.jpg', 'Goat!', 'Photo of a goat taken at Glacier National Park.');
+('images/goat_small.jpg', 'Goat!', 'Photo of a goat taken at Glacier National Park.'),
+('images/market.JPG', 'Market in Guatemala!', 'Photo of an open air market in Guatemala.'),
+('images/portugal.jpg', 'Portugal!', 'Photo of a sunset taken on the Azores islands in Portugal.'),
+('images/pretzel.jpeg', 'Pretzel!', 'Photo of a delicious German soft pretzel.'),
+('images/skiing.jpg', 'XC Skiing!', 'Photo of Kristin skiing in the Kortelopet ski race in Hayward, WI.'),
+('images/tikal.JPG', 'Tikal!', 'Photo of a temple in Tikal.');
   
